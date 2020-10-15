17e1115e104b1b8a9d9167b76588e4e0d437f9f4
StefanoSerafini <stefanoserafini7@gmail.com>
2020-10-15 15:55:04 +0200

    stampa a video ris dopo controllo

--- myscript.js
+++ myscript.js
@@ -1,19 +1,27 @@
 
 //1° es richiesta email e convalida se presente
 
-var email = ["michele@boolean.careers", "fabio@boolean.careers", "roberto@boolean.careers"];
+var email = ["a@boolean.careers", "b@boolean.careers", "c@boolean.careers"];
 
 console.log(email);
 
-var newEmail = prompt('Log in with your e-mail please').toLowerCase();
+var newEmail = prompt('Inserisci la tua mail').toLowerCase();
 console.log(newEmail);
 
 var emailtf = false;
-
+//ciclo for per controllo presenza mailmail
 for (var i = 0; i < email.length; i++) {
   var check = email[i];
 
   if (check == newEmail) {
-    emailtf = true;
+    var emailtf = true;
   }
+}
+//ciclo if per stampa sia console che a schermo
+if (emailtf === true) {
+  console.log('Login done: ' + newEmail);
+  document.getElementById('emaildone').innerHTML = 'Login effettuato: ' + newEmail;
+} else {
+  console.log('email non corretta: ' + newEmail);
+  document.getElementById('emaildone').innerHTML = 'email non corretta: ' + newEmail;
 }
